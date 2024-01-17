24f3e0c53c9a7635d61c5179967e420e2ffb6f08
edweb77 <dwehus@gmx.de>
2024-01-17 13:27:09 +0100

    fahrparcours.py hinzugefügt

--- basisklassen.py
+++ basisklassen.py
@@ -11,7 +11,7 @@
 
 import click
 import time
-import numpy as np
+import numpy as nppberry
 import math
 import RPi.GPIO as GPIO
 import smbus
---
+++ soniccar.py
@@ -0,0 +1,9 @@
+from basecar import *
+
+class SonicCar(BaseCar):
+    """Klasse zur Erweiterung der Basisklasse um den Ultraschallsensor
+    
+    """
+    def __init__(self) -> None:
+        super().__init__()
+    
