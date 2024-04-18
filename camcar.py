f843d79424c0ddd45592f53106855f7bfee0bb01
FuPaS <fupas@freenet.de>
2024-04-18 10:03:31 +0200

    Normalisierung

--- camcar.py
+++ camcar.py
@@ -1,7 +1,7 @@
 from ir_car import * 
 from basisklassen_cam import Camera
 import cv2
-from fahrparcours_dash import *
+#from fahrparcours_dash import *
 
 class CamCar(IRCar):
     
@@ -14,18 +14,19 @@ class CamCar(IRCar):
         # Kamera-Objekt liefert aktuelles Bild als Numpy-Array
         frame = self.camera.get_frame()
         # Einige beipielhafte Manipulationen des Bildes
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.normalize(frame, frame, alpha=2, norm_type=cv2.NORM_MINMAX)
         # canny = cv2.Canny(gray, 100, 200)
-        frame = gray
         frame = frame[150:350,0:640].copy()
         imgTemplate = frame[100:170,50:540].copy()
-        while fahren:
+
+        while True:
             # Kamera-Objekt liefert aktuelles Bild als Numpy-Array
             frame = self.camera.get_frame()
             # Einige beipielhafte Manipulationen des Bildes
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.normalize(frame, frame, alpha=2, norm_type=cv2.NORM_MINMAX)
             # canny = cv2.Canny(gray, 100, 200)
-            frame = gray
             frame = frame[200:480,0:640].copy()
             res = cv2.matchTemplate(frame, imgTemplate,cv2.TM_SQDIFF) 
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
@@ -41,7 +42,7 @@ class CamCar(IRCar):
             _, x = cv2.imencode(".jpeg", img3)
             x_bytes = x.tobytes()
 
-
+            
             self.steering_angle = 0.9 * x_position + 45
             # if x_position < 25: 
             #     self.steering_angle = 45
